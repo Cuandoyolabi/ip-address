12f0ce8179bfd0c16b20d476f4907b9b2dfc3476
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-07-15 11:03:30 -0600

    Se da inicio al proyecto de la ubicacion mediante Ip, ya hay archivos creados y la mayoria del css y estructura, esta lista, se empezara con la creacion de la logica

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const id = document.getElementById("information-id");
+const location = document.getElementById("information-location");
+const timezone = document.getElementById("information-timezone");
+const isp = document.getElementById("information-isp");
+
+
+
+
+
