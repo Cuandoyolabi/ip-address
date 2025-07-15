14874382f972821091291ce3847883c2ac235739
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-07-15 13:03:29 -0600

    Se va implementando la API poco a poco

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
+
+const API_KEY = "at_uEtGTV9rwWx4mSjwrvfiExLvFSmrB";
+const API = "https://geo.ipify.org/api/v2/country";
+const fullURL = `${API}?apikey=${API_KEY}&ipAddress=8.8.8.8`;
+
+const fullPrueba = "https://geo.ipify.org/api/v2/country?apiKey=at_uEtGTV9rwWx4mSjwrvfiExLvFSmrB&ipAddress=8.8.8.8";
+
 const id = document.getElementById("information-id");
-const location = document.getElementById("information-location");
-const timezone = document.getElementById("information-timezone");
+const locationZone = document.getElementById("information-location");
+const timeZone = document.getElementById("information-timezone");
 const isp = document.getElementById("information-isp");
 
 
+const solicitud = fetch(fullPrueba)
+    .then(response => response.json())
+    .then(data => { 
+        console.log(data)
+        console.log(data.ip)
+        id.textContent = data.ip;
+        locationZone.textContent = data.location.region;
+        timeZone.textContent = data.location.timezone;
+        isp.textContent = data.isp;
+    })
 
+    
 
 
